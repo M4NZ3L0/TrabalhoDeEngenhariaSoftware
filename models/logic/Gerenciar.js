a192aa46a4fd510841f04885ed7b458a58a6498c
M4NZ3L0 <marcelinhom1plays@gmail.com>
2021-11-23 12:53:38 -0300

    almoço

--- models/logic/Gerenciar.js
+++ models/logic/Gerenciar.js
@@ -99,14 +99,23 @@ let GetInstrumentos = async () => {
     InstrumentosRender.splice(0, InstrumentosRender.length);
 
     await data.forEach(e => {
-        return InstrumentosRender.push(`<tr>
-    <td id="id">${e.id}</td>
-                        <td id="nome">${e.Nome}</td>
-                        <td id="imagem">${e.Imagem}</td>
-                        <td id="codigo">${e.Código}</td>
-                        <td id="tipo">${e.Tipo}</td>
-                        <td id="tamanhocms">${e.TamanhoCms}</td>
-                        <td id="pesokgs">${e.PesoKgs}</td>
+        return InstrumentosRender.push(`<tr class="bg-gray-100">
+    <td class="text-center text-gray-900 font-bold" id="id">${e.id}</td>
+                        <td class="text-center text-gray-800" id="nome">${e.Nome}</td>
+                        <td class="text-center text-gray-800" id="codigo">${e.Código}</td>
+                        <td class="text-center text-gray-800" id="tipo">${e.Tipo}</td>
+                        <td class="text-center text-gray-800" id="tamanhocms">${e.TamanhoCms}</td>
+                        <td class="text-center text-gray-800" id="pesokgs">${e.PesoKgs}</td>
+                        <td><div class="flex justify-center items-center">
+                        <a class="m-4" href="atualizarCurso/${e.id}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-900" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+</svg></a>
+                        <button class="m-4 delete" data-id="${e.id}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-900" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+</svg></button>
+                        </div></td>
                     </tr>`)
     });
 };
--- models/logic/GerenciarFunções.js
+++ models/logic/GerenciarFunções.js
@@ -89,4 +89,103 @@ export const DeletarProfessor = async (req,res) => {
         axios.delete(deleteurl)
             .then(() => res.redirect(`http://localhost:5050/gerenciarcursos`))
             .catch(err => console.log(err));
-    }
+}
+    
+
+
+
+//#Alunos
+
+
+export const CriarAluno = async(req, res) => {
+        const obj = {
+            curso: req.body.curso,
+            professores: req.body.professores,
+            diasdeaula: req.body.diasdeaula,
+            horarios:req.body.horarios,
+            alunosmatriculados: req.body.alunosmatriculados
+        };
+
+        axios.post("http://localhost:5050/api/cursos", obj)
+            .then(() => res.redirect(req.originalUrl))
+            .catch(err => console.log(err));
+}
+
+//#Professores
+export const AtualizarAluno = async (req, res) => {
+    
+    const obj = {
+            id:req.params.id,
+            curso: req.body.curso,
+            professores: req.body.professores,
+            diasdeaula: req.body.diasdeaula,
+            horarios:req.body.horarios,
+            alunosmatriculados: req.body.alunosmatriculados
+    };
+    
+        const updateurl = `http://localhost:5050/api/cursos/${req.params.id}`
+    
+        axios.put(updateurl, obj)
+            .then(() => res.redirect(`http://localhost:5050/gerenciarcursos`))
+            .catch(err => console.log(err));
+}
+    
+   
+export const DeletarAluno = async (req,res) => {
+    
+    const id = req.params.id;
+    console.log(id);
+    const deleteurl = `http://localhost:5050/api/cursos/${id}`;
+    
+        axios.delete(deleteurl)
+            .then(() => res.redirect(`http://localhost:5050/gerenciarcursos`))
+            .catch(err => console.log(err));
+}
+
+//#instruments
+
+
+export const CriarInstrumentos = async(req, res) => {
+        const obj = {
+            curso: req.body.curso,
+            professores: req.body.professores,
+            diasdeaula: req.body.diasdeaula,
+            horarios:req.body.horarios,
+            alunosmatriculados: req.body.alunosmatriculados
+        };
+
+        axios.post("http://localhost:5050/api/cursos", obj)
+            .then(() => res.redirect(req.originalUrl))
+            .catch(err => console.log(err));
+}
+
+//#Professores
+export const AtualizarInstrumentos = async (req, res) => {
+    
+    const obj = {
+            id:req.params.id,
+            curso: req.body.curso,
+            professores: req.body.professores,
+            diasdeaula: req.body.diasdeaula,
+            horarios:req.body.horarios,
+            alunosmatriculados: req.body.alunosmatriculados
+    };
+    
+        const updateurl = `http://localhost:5050/api/cursos/${req.params.id}`
+    
+        axios.put(updateurl, obj)
+            .then(() => res.redirect(`http://localhost:5050/gerenciarcursos`))
+            .catch(err => console.log(err));
+}
+    
+   
+export const DeletarInstrumentos = async (req,res) => {
+    
+    const id = req.params.id;
+    console.log(id);
+    const deleteurl = `http://localhost:5050/api/cursos/${id}`;
+    
+        axios.delete(deleteurl)
+            .then(() => res.redirect(`http://localhost:5050/gerenciarcursos`))
+            .catch(err => console.log(err));
+}
